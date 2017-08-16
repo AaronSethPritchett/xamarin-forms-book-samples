cea922be599fd73532e8e205f96ec10aae94dd47
Charles Petzold <chape@microsoft.com>
2017-08-16 14:56:14 -0400

    [macos] Chapter 14 Tryout

---
+++ Chapter14/ChessboardProportional/ChessboardProportional/ChessboardProportional.MacOS/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace ChessboardProportional.MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
